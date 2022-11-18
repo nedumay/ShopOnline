881ebb0516cf069ade415fd4acb1a4a58421b557
Samim <Samim058@gamil.com>
2022-11-18 15:58:51 +0300

    Refactor

--- app/src/main/java/com/example/shoponline/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/shoponline/presentation/main/MainActivity.kt
@@ -3,26 +3,114 @@ package com.example.shoponline.presentation.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.CheckBox
 import android.widget.Spinner
 import com.example.shoponline.R
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var spinnerCity: Spinner
+    lateinit var checkPhone: CheckBox
+    lateinit var checkComputer: CheckBox
+    lateinit var checkHealth: CheckBox
+    lateinit var checkBooks: CheckBox
+    lateinit var checkOther: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val city_menu = resources.getStringArray(R.array.city)
-        spinnerCity = findViewById(R.id.choice_city)
+        // выпадающий список городов
+        val spinnerCity = findViewById<Spinner>(R.id.choice_city)
+        adapterSpinnerCity(spinner = spinnerCity)
+
+        checkPhone = findViewById(R.id.btn_check_phone)
+        checkPhone.isChecked = true
+
+        checkComputer = findViewById(R.id.btn_check_computer)
+        checkHealth = findViewById(R.id.btn_check_health)
+        checkBooks = findViewById(R.id.btn_check_books)
+        checkOther = findViewById(R.id.btn_check_other)
+        // Переключение кнопок check
+        checkBoxMenu()
+
+
+    }
+
+    private fun adapterSpinnerCity(spinner: Spinner) {
         ArrayAdapter.createFromResource(
             this,
             R.array.city,
             android.R.layout.simple_list_item_1
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinnerCity.adapter = adapter
+        ).also { adapterSpinner ->
+            adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapterSpinner
+        }
+    }
+
+    private fun checkBoxMenu() {
+        // Кнопка Phone
+        checkPhone.setOnCheckedChangeListener { compoundButton, isChecked ->
+            if(isChecked){
+                checkPhone.isChecked = true
+                checkComputer.isChecked = false
+                checkBooks.isChecked = false
+                checkHealth.isChecked = false
+                checkOther.isChecked = false
+            } else {
+                checkPhone.isChecked = false
+            }
+        }
+
+        // Кнопка Computer
+        checkComputer.setOnCheckedChangeListener { compoundButton, isChecked ->
+            if(isChecked){
+                checkPhone.isChecked = false
+                checkComputer.isChecked = true
+                checkBooks.isChecked = false
+                checkHealth.isChecked = false
+                checkOther.isChecked = false
+            } else {
+                checkComputer.isChecked = false
+            }
+        }
+
+        // Кнопка Books
+        checkBooks.setOnCheckedChangeListener { compoundButton, isChecked ->
+            if(isChecked){
+                checkPhone.isChecked = false
+                checkComputer.isChecked = false
+                checkBooks.isChecked = true
+                checkHealth.isChecked = false
+                checkOther.isChecked = false
+            } else {
+                checkBooks.isChecked = false
+            }
+        }
+
+        // Кнопка Health
+        checkHealth.setOnCheckedChangeListener { compoundButton, isChecked ->
+            if(isChecked){
+                checkPhone.isChecked = false
+                checkComputer.isChecked = false
+                checkBooks.isChecked = false
+                checkHealth.isChecked = true
+                checkOther.isChecked = false
+            } else {
+                checkHealth.isChecked = false
+            }
+        }
+
+        // Кнопка Other
+        checkOther.setOnCheckedChangeListener { compoundButton, isChecked ->
+            if(isChecked){
+                checkPhone.isChecked = false
+                checkComputer.isChecked = false
+                checkBooks.isChecked = false
+                checkHealth.isChecked = false
+                checkOther.isChecked = true
+            } else {
+                checkOther.isChecked = false
+            }
         }
     }
 }
